8f1f7c9e74e1b333e8422226397f2a41ae831d40
Akshat Kumar <akshat1028kumar@gmail.com>
2025-09-25 00:11:33 +0530

    upgraded version of expo to sdk 54

--- metro.config.js
+++ metro.config.js
@@ -2,6 +2,4 @@ const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname, { isCSSEnabled: true });
-config.resolver.unstable_enablePackageExports = false;
-config.resolver.unstable_enableSymlinks = false;
 module.exports = withNativeWind(config, { input: './global.css' });
